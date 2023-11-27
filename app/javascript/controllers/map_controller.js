2cd0535c5561aa9995a1879c17548393c731ae22
Michael Sam <sam.michael.h@gmail.com>
2023-11-27 12:40:09 +0900

    Fix the direction?

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
   }
 
   connect() {
-    console.log(this.markersValue)
+    // console.log(this.markersValue)
     mapboxgl.accessToken = this.apiKeyValue;
 
     this.map = new mapboxgl.Map({
@@ -19,15 +19,23 @@ export default class extends Controller {
       zoom: 13,
     });
 
-    this.addMarkers();
-    this.fitMapToMarkers();  // Call the function to fit map to markers
-    this.renderDirection();
+    this.closestPlace = [this.markersValue[0].lng, this.markersValue[0].lat];
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.position = position;
+        this.trackUserLocationStart();
+        this.addMarkers();
+        this.addDirectionControl();
+      });
+    }
   }
 
   addMarkers() {
+    console.log("Add markers")
     this.markersValue.forEach(marker => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html);
-      console.log(marker)
+
       const customMarker = document.createElement("div");
       customMarker.innerHTML = marker.marker_html;
 
@@ -39,37 +47,59 @@ export default class extends Controller {
   }
 
   fitMapToMarkers() {
+    console.log("Fit markers")
     const bounds = new mapboxgl.LngLatBounds();
-    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    bounds.extend([this.position.coords.longitude, this.position.coords.latitude]);
+    console.log([this.position.coords.longitude, this.position.coords.latitude]);
+    // Code below display the closest marker to the user's position
+    bounds.extend(this.closestPlace);
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 200 });
   }
 
-  renderDirection() {
-    let direction
+  trackUserLocationStart() {
+    console.log("Track user")
+    // Initialize the GeolocateControl.
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+          enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    });
+    // Add the control to the map.
+    this.map.addControl(geolocate);
+    // Set an event listener that fires
+    this.map.on('load', () => {
+      geolocate.trigger();
+      });
+    // when a trackuserlocationstart event occurs. This code below is the origin in renderDirection
+    geolocate.on('geolocate', (position) => {
+      console.log('A geolocate event has occurred.');
+      // This code below is the destination in renderDirection
+      this.renderDirection(position, this.closestPlace);
+    });
+  }
 
-    if (direction) {
-      this.map.removeControl(direction)
-    }
-    direction = new MapboxDirections({
+  renderDirection(origin, destination) {
+    // console.log(destination)
+
+    this.direction.setOrigin([origin.coords.longitude, origin.coords.latitude]);
+    this.direction.setDestination(destination);
+  }
+
+  addDirectionControl() {
+    this.direction = new MapboxDirections({
         accessToken: mapboxgl.accessToken,
         routePadding: 50,
+        profile: "mapbox/walking",
         controls: {
           inputs: false,
-          instructions: false
+          instructions: false,
         }
       })
     this.map.addControl(
-      direction,
+      this.direction,
       'top-left'
     );
-
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        console.log([position.coords.longitude, position.coords.latitude])
-        direction.setOrigin([position.coords.longitude, position.coords.latitude])
-        direction.setDestination([139.70811773947588, 35.634269435296645])
-      });
-    }
   }
 
 }
